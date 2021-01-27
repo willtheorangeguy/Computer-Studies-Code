f8c1a35c3fca66886dea6a07c7e1a10e289a46ff
William <willtheorangeguy@users.noreply.github.com>
2021-01-27 14:40:57 -0800

    Fix hash

--- Python Challenges/Better_Password_Prompt.py
+++ Python Challenges/Better_Password_Prompt.py
@@ -8,17 +8,15 @@ logged_in = False
 
 # Ask for New Password
 pwd = getpass.getpass("Enter new password: ")
-hash_1 = hashlib.md5().update(pwd.encode('utf8')) 
+hash_1 = hashlib.md5().update(pwd.encode('utf8'))
 
 # Confirm Password
 while not(logged_in):
     login = getpass.getpass("Enter password: ")
-    hash_2 = hashlib.md5().update(login.encode('utf8')) 
-    if str(hash_1) == str(hash_2):
+    hash_2 = hashlib.md5().update(login.encode('utf8'))
+    if hash_1 == hash_2:
         print("You got it!")
         logged_in = True
     else:
         print("Sorry, that did not match. Please try again.")
         continue
-
-
